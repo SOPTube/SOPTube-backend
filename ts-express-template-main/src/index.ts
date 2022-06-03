c25a118045a05b818f522f9e5628d48f41f5fce6
tkarndbrtk <seohy1013@kw.ac.kr>
2022-06-03 22:19:47 +0900

    [FIX] cors 수정

--- ts-express-template-main/src/index.ts
+++ ts-express-template-main/src/index.ts
@@ -7,42 +7,11 @@ const cors = require("cors");
 
 connectDB();
 
-let corsOptions_server = {
-  origin: "http://13.209.5.193:8000",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-let corsOptions_local1 = {
-  origin: "http://localhost:8000",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-let corsOptions_local2 = {
-  origin: "http://localhost:3000",
-  credentials: true,
-<<<<<<< HEAD
-  optionsSuccessStatus: 200
-}
-
-app.use(cors(corsOptions_server1));
-app.use(cors(corsOptions_server2));
-=======
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions_server));
->>>>>>> 92c017b820a576d7fcef049f4654f0bb9169f984
-app.use(cors(corsOptions_local1));
-app.use(cors(corsOptions_local2));
-
+app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, content-type, x-access-token"
-  );
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
   next();
 });
 
