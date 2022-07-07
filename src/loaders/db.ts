cf62ef18f3da39ca5576ebcb0cae2ebb7ee847b6
heerucan <iwpow0307@gmail.com>
2022-07-07 12:32:16 +0900

    [CHORE] buildspec.yml 수정

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -5,26 +5,26 @@ import User from "../models/User";
 import Video from "../models/Video";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(config.mongoURI);
+  try {
+    await mongoose.connect(config.mongoURI);
 
-        await User.createCollection().then(() => {
-            console.log("User Collection is created!");
-        })
-        await RecommendVideo.createCollection().then(() => {
-            console.log("RecommendVideo Collection is created!");
-        })
-        await Video.createCollection().then(() => {
-            console.log("Video Collection is created!");
-        })
-        
-        mongoose.set('autoCreate', true);
+    await User.createCollection().then(() => {
+      console.log("User Collection is created!");
+    });
+    await RecommendVideo.createCollection().then(() => {
+      console.log("RecommendVideo Collection is created!");
+    });
+    await Video.createCollection().then(() => {
+      console.log("Video Collection is created!");
+    });
 
-        console.log("Mongoose Connected ...");
-    } catch (err: any) {
-        console.error(err.message);
-        process.exit(1);
-    }
+    mongoose.set("autoCreate", true);
+
+    console.log("Mongoose Connected ...");
+  } catch (err: any) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
